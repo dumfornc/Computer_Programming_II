9d9889a2ff15240b485ac5adaa5652c3b4a432d4
dumfornc <dumfornc@mail.uc.edu>
2025-08-30 18:48:59 -0400

    [Pract01] Creating initial files and framework for practicum 01.

---
+++ Practicum_01/src/PersonGenerator.java
@@ -0,0 +1,7 @@
+public class PersonGenerator
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
---
+++ Practicum_01/src/PersonReader.java
@@ -0,0 +1,7 @@
+public class PersonReader
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
