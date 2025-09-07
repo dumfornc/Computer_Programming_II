bfeb1a7608180d10ff27b30ed95d74fe2ccb1dba
dumfornc <dumfornc@mail.uc.edu>
2025-09-07 01:46:32 -0400

    [Lab01] Finished refactor of PersonGenerator.java to use Person object.

--- Lab_01/src/PersonGenerator.java
+++ Lab_01/src/PersonGenerator.java
@@ -11,11 +11,7 @@ public class PersonGenerator
 {
     public static void main(String[] args)
     {
-        ArrayList<String> peopleIDs = new ArrayList<String>();
-        ArrayList<String> peopleFirstNames = new ArrayList<String>();
-        ArrayList<String> peopleLastNames = new ArrayList<String>();
-        ArrayList<String> peopleTitles = new ArrayList<String>();
-        ArrayList<Integer> peopleBirthYears = new ArrayList<Integer>();
+        ArrayList<Person> people = new ArrayList<Person>();
 
         Scanner in = new Scanner(System.in);
 
@@ -27,29 +23,15 @@ public class PersonGenerator
         while (morePersons)
         {
             personCount += 1;
-            peopleIDs.add(SafeInput.getRegExString(in, "Enter the ID (######) of person " + personCount, "\\d{6}"));
-            peopleFirstNames.add(SafeInput.getNonZeroLenString(in, "Enter the first name of person " + personCount));
-            peopleLastNames.add(SafeInput.getNonZeroLenString(in, "Enter the last name of person " + personCount));
-            peopleTitles.add(SafeInput.getNonZeroLenString(in, "Enter the title or honorific of person " + personCount));
-            peopleBirthYears.add(SafeInput.getRangedInt(in, "Enter the birth year of person " + personCount, 1000, 1200));
+            String id = SafeInput.getRegExString(in, "Enter the ID (######) of person " + personCount, "\\d{6}");
+            String firstName = SafeInput.getNonZeroLenString(in, "Enter the first name of person " + personCount);
+            String lastName = SafeInput.getNonZeroLenString(in, "Enter the last name of person " + personCount);
+            String title = SafeInput.getNonZeroLenString(in, "Enter the title or honorific of person " + personCount);
+            int yob = SafeInput.getRangedInt(in, "Enter the birth year of person " + personCount, 1940, 2010);
+
+            people.add(new Person(id, firstName, lastName, title, yob));
 
             morePersons = SafeInput.getYNConfirm(in, "Do you want to enter data for another person");
-        }
-
-        // Below was copied and only slightly cleaned up from NIOWriteTextFile.java
-        ArrayList<String> fileLines = new ArrayList<String>();
-        String personFormat = "%s, %s, %s, %s, %s";
-
-        for (int personIndex = 0; personIndex < personCount; personIndex++)
-        {
-            fileLines.add(personFormat.formatted
-            (
-                peopleIDs.get(personIndex),
-                peopleFirstNames.get(personIndex),
-                peopleLastNames.get(personIndex),
-                peopleTitles.get(personIndex),
-                peopleBirthYears.get(personIndex)
-            ));
         }
 
         File workingDirectory = new File(System.getProperty("user.dir"));
@@ -64,8 +46,9 @@ public class PersonGenerator
 
             // Finally can write the file LOL!
 
-            for(String line : fileLines)
+            for(Person person : people)
             {
+                String line = person.toCSV();
                 writer.write(line, 0, line.length());  // stupid syntax for write rec
                 // 0 is where to start (1st char) the write
                 // rec. length() is how many chars to write (all)
