10711cba8b511a0dddff237f1f3bf247d7bc5b49
dumfornc <dumfornc@mail.uc.edu>
2025-09-29 23:05:57 -0400

    [Lab04] First completed draft of the app. The image is too large, the textarea is clipping off the sides, and there needs to be some general cleanup but it is generally functional.

--- Lab_04/src/FortuneTellerFrame.java
+++ Lab_04/src/FortuneTellerFrame.java
@@ -1,14 +1,25 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.WindowEvent;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class FortuneTellerFrame extends JFrame
 {
     private ArrayList<String> fortunes = new ArrayList<String>();
+    private JTextArea fortuneTextArea = new JTextArea(24, 50);
+    private Random randomFortuneIndex = new Random();
+    private int lastFortuneIndex = -1;
 
     public FortuneTellerFrame() throws HeadlessException
     {
         this.setFortunes();
+
+        setTitle("Fortune Teller");
+        setSize(600, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        this.initializeUI();
     }
 
     private void setFortunes()
@@ -25,5 +36,84 @@ public class FortuneTellerFrame extends JFrame
         this.fortunes.add("All shall find the light at last");
         this.fortunes.add("If you fail to hydrate today bad things will happen");
         this.fortunes.add("You will pass a class in the next 2 years");
+    }
+
+    private void initializeUI()
+    {
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BorderLayout());
+
+        JPanel topPanel = initializeTopPanel();
+        JPanel middlePanel = initializeMiddlePanel();
+        JPanel bottomPanel = initializeBottomPanel();
+
+        mainPanel.add(topPanel, BorderLayout.PAGE_START);
+        mainPanel.add(middlePanel, BorderLayout.CENTER);
+        mainPanel.add(bottomPanel, BorderLayout.PAGE_END);
+
+        add(mainPanel);
+    }
+
+    private JPanel initializeTopPanel()
+    {
+        JPanel topPanel = new JPanel();
+        ImageIcon fortuneTellerImage = new ImageIcon("Images\\FortuneTeller.png");
+
+        JLabel title = new JLabel("Are You Ready to Learn the Future?", fortuneTellerImage, JLabel.CENTER);
+        title.setHorizontalTextPosition(JLabel.CENTER);
+        title.setVerticalTextPosition(JLabel.BOTTOM);
+
+        Font topPanelFont = new Font("Gabriola", Font.PLAIN, 50);
+        title.setFont(topPanelFont);
+
+        topPanel.add(title);
+        return topPanel;
+    }
+
+    private JPanel initializeMiddlePanel()
+    {
+        JPanel middlePanel = new JPanel(new BorderLayout());
+
+        Font middlePanelFont = new Font("Calibri", Font.PLAIN, 16);
+        this.fortuneTextArea.setFont(middlePanelFont);
+
+        JScrollPane scroller = new JScrollPane(this.fortuneTextArea, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        middlePanel.add(scroller, BorderLayout.CENTER);
+
+        return middlePanel;
+    }
+
+    private JPanel initializeBottomPanel()
+    {
+        JPanel bottomPanel = new JPanel();
+        bottomPanel.setLayout(new BorderLayout());
+
+        JButton fortuneButton = new JButton("Read My Fortune!");
+        fortuneButton.addActionListener((x) -> {this.fortuneTextArea.append(this.getFortune() + "\n");});
+        bottomPanel.add(fortuneButton, BorderLayout.LINE_START);
+
+        JButton quitButton = new JButton("Quit");
+        quitButton.addActionListener((x) -> {dispatchEvent(new WindowEvent(this, WindowEvent.WINDOW_CLOSING));});
+        bottomPanel.add(quitButton, BorderLayout.LINE_END);
+
+        return bottomPanel;
+    }
+
+    public String getFortune()
+    {
+        String newFortune = "";
+
+        while (newFortune.isEmpty())
+        {
+            int newIndex = this.randomFortuneIndex.nextInt(12);
+
+            if (newIndex != this.lastFortuneIndex)
+            {
+                newFortune = this.fortunes.get(newIndex);
+                this.lastFortuneIndex = newIndex;
+            }
+        }
+
+        return newFortune;
     }
 }
--- Lab_04/src/FortuneTellerViewer.java
+++ Lab_04/src/FortuneTellerViewer.java
@@ -1,14 +1,11 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class FortuneTellerViewer
 {
     public static void main(String[] args)
     {
-        FortuneTellerFrame frame = new FortuneTellerFrame();
-
-        frame.setTitle("Fortune Teller");
-        frame.setSize(400, 400);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JFrame frame = new FortuneTellerFrame();
         frame.setVisible(true);
     }
 }
