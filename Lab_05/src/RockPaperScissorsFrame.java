6c252d14fba1ea0ec9899dd1d9342072bf683f17
dumfornc <dumfornc@mail.uc.edu>
2025-10-07 00:46:44 -0400

    [Lab05] Have a working game with the basic radom selection.

--- Lab_05/src/RockPaperScissorsFrame.java
+++ Lab_05/src/RockPaperScissorsFrame.java
@@ -1,7 +1,10 @@
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.WindowEvent;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class RockPaperScissorsFrame extends JFrame
 {
@@ -10,10 +13,19 @@ public class RockPaperScissorsFrame extends JFrame
     private int playerPaperUses = 0;
     private int playerScissorsUses = 0;
 
+    private int playerWins = 0;
+    private int ties = 0;
+    private int computerWins = 0;
+
     //Constants used internally to represent the different options
-    public static final int ROCK = 1;
-    public static final int PAPER = 2;
-    public static final int SCISSORS = 3;
+    public static final int ROCK = 0;
+    public static final int PAPER = 1;
+    public static final int SCISSORS = 2;
+
+    public static final String[] optionNames = {"Rock", "Paper", "Scissors"};
+
+    //Array indexed by the constant values that holds the words used to describe that option winning
+    public static final String[] victoryWords = {" crushes ", " covers ", " cuts "};
 
     //Declaring text fields as instance variables so that they can easily be updated
     private JTextField playerWinsField = new JTextField();
@@ -21,6 +33,9 @@ public class RockPaperScissorsFrame extends JFrame
     private JTextField computerWinsField = new JTextField();
 
     private JTextArea historyText = new JTextArea();
+
+    //A random number generator to generate the random response of the program
+    private Random programResponseGenerator = new Random();
 
     public RockPaperScissorsFrame()
     {
@@ -102,13 +117,13 @@ public class RockPaperScissorsFrame extends JFrame
         JPanel buttonPanel = new JPanel();
         buttonPanel.setLayout(new GridLayout(1,4));
 
-        JButton rockButton = createRPSButton("imgs\\Rock.png", "Rock");
+        JButton rockButton = createRPSButton("imgs\\Rock.png", "Rock", ROCK);
         buttonPanel.add(rockButton);
 
-        JButton paperButton = createRPSButton("imgs\\Paper.png", "Paper");
+        JButton paperButton = createRPSButton("imgs\\Paper.png", "Paper", PAPER);
         buttonPanel.add(paperButton);
 
-        JButton scissorsButton = createRPSButton("imgs\\Scissors.png", "Scissors");
+        JButton scissorsButton = createRPSButton("imgs\\Scissors.png", "Scissors", SCISSORS);
         buttonPanel.add(scissorsButton);
 
         JButton quitButton = new JButton("Quit");
@@ -120,13 +135,132 @@ public class RockPaperScissorsFrame extends JFrame
         return buttonPanel;
     }
 
-    private JButton createRPSButton(String imagePath, String buttonText)
+    private JButton createRPSButton(String imagePath, String buttonText, int actionEnum)
     {
         ImageIcon buttonImage = new ImageIcon(imagePath);
         JButton newButton = new JButton(buttonText, buttonImage);
+
         newButton.setHorizontalTextPosition(JButton.CENTER);
         newButton.setVerticalTextPosition(JButton.BOTTOM);
 
+        newButton.addActionListener((e) -> {this.playGame(e, actionEnum);});
+
         return newButton;
+    }
+
+    private void playGame(ActionEvent e, int playerChoice)
+    {
+        this.incrementPlayerChoice(playerChoice);
+
+        this.determineOutcome(playerChoice, this.determineComputerChoice());
+    }
+
+    private void incrementPlayerChoice(int playerChoice)
+    {
+        if(playerChoice == ROCK)
+        {
+            this.playerRockUses++;
+        }
+        else if(playerChoice == PAPER)
+        {
+            this.playerPaperUses++;
+        }
+        else if(playerChoice == SCISSORS)
+        {
+            this.playerScissorsUses++;
+        }
+    }
+
+    private int determineComputerChoice()
+    {
+        return this.programResponseGenerator.nextInt(0, 3);
+    }
+
+    private void determineOutcome(int playerChoice, int computerChoice)
+    {
+        if(playerChoice == computerChoice)
+        {
+            handleTie(playerChoice, computerChoice);
+        }
+        else if(playerChoice == ROCK)
+        {
+            if(computerChoice == PAPER)
+            {
+                handleComputerWin(PAPER);
+            }
+            else if(computerChoice == SCISSORS)
+            {
+                handlePlayerWin(ROCK);
+            }
+        }
+        else if(playerChoice == PAPER)
+        {
+            if(computerChoice == SCISSORS)
+            {
+                handleComputerWin(SCISSORS);
+            }
+            else if(computerChoice == ROCK)
+            {
+                handlePlayerWin(PAPER);
+            }
+        }
+        else if(playerChoice == SCISSORS)
+        {
+            if(computerChoice == ROCK)
+            {
+                handleComputerWin(ROCK);
+            }
+            else if(computerChoice == PAPER)
+            {
+                handlePlayerWin(SCISSORS);
+            }
+        }
+    }
+
+    private void handleTie(int playerChoice, int computerChoice)
+    {
+        String tieMessage = optionNames[playerChoice] + " ties " + optionNames[computerChoice] + "(No Winner)\n";
+        this.historyText.append(tieMessage);
+
+        this.ties++;
+        this.tiesField.setText(String.valueOf(this.ties));
+    }
+
+    private void handleComputerWin(int computerChoice)
+    {
+        String computerWinMessage = createVictoryMessage(computerChoice) + " (Computer Wins)\n";
+        this.historyText.append(computerWinMessage);
+
+        this.computerWins++;
+        this.computerWinsField.setText(String.valueOf(this.computerWins));
+    }
+
+    private void handlePlayerWin(int playerChoice)
+    {
+        String playerWinMessage = createVictoryMessage(playerChoice) + " (Player Wins)\n";
+        this.historyText.append(playerWinMessage);
+
+        this.playerWins++;
+        this.playerWinsField.setText(String.valueOf(this.playerWins));
+    }
+
+    private String createVictoryMessage(int victorChoice)
+    {
+        String winMessage = "";
+
+        if(victorChoice == ROCK)
+        {
+            winMessage = optionNames[ROCK] + victoryWords[ROCK] + optionNames[SCISSORS];
+        }
+        else if(victorChoice == PAPER)
+        {
+            winMessage = optionNames[PAPER] + victoryWords[PAPER] + optionNames[ROCK];
+        }
+        else if(victorChoice == SCISSORS)
+        {
+            winMessage =optionNames[SCISSORS] + victoryWords[SCISSORS] + optionNames[PAPER];
+        }
+
+        return winMessage;
     }
 }
