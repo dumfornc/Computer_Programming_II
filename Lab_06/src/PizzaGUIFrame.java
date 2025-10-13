e3e5de9365d811f9dd3cf5df6ea93003ef6c3d9b
dumfornc <dumfornc@mail.uc.edu>
2025-10-12 20:10:11 -0400

    [Lab06] All components other than buttons have been created.

--- Lab_06/src/PizzaGUIFrame.java
+++ Lab_06/src/PizzaGUIFrame.java
@@ -1,8 +1,11 @@
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
 public class PizzaGUIFrame extends JFrame
 {
+    private JTextArea resultsText;
+
     PizzaGUIFrame()
     {
         setTitle("Pizza Order Form");
@@ -19,5 +22,147 @@ public class PizzaGUIFrame extends JFrame
         setLocation(screenWidth / 4, screenHeight / 4);
 
         this.initializeUI();
+    }
+
+    private void initializeUI()
+    {
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new GridLayout(3, 1));
+
+        JPanel topPanel = initializeOrderPanel();
+        JPanel middlePanel = initializeResultsPanel();
+        JPanel bottomPanel = initializeButtonsPanel();
+
+        mainPanel.add(topPanel);
+        mainPanel.add(middlePanel);
+        mainPanel.add(bottomPanel);
+
+        this.add(mainPanel);
+    }
+
+    private JPanel initializeOrderPanel()
+    {
+        JPanel orderPanel = new JPanel();
+        orderPanel.setLayout(new GridLayout(1, 2));
+
+        JPanel topLeftPanel = initializePizzaTypePanel();
+        JPanel topRightPanel = initializePizzaToppingsPanel();
+
+        orderPanel.add(topLeftPanel);
+        orderPanel.add(topRightPanel);
+
+        return orderPanel;
+    }
+
+    private JPanel initializePizzaTypePanel()
+    {
+        JPanel pizzaTypePanel = new JPanel();
+        pizzaTypePanel.setLayout(new GridLayout(1, 2));
+        pizzaTypePanel.setBorder(BorderFactory.createTitledBorder(
+                BorderFactory.createLoweredBevelBorder(),
+                "Choose Your Pizza Type:"
+        ));
+
+
+        JPanel leftPanel = initializePizzaSizePanel();
+        JPanel rightPanel = initializeCrustTypePanel();
+
+        pizzaTypePanel.add(leftPanel);
+        pizzaTypePanel.add(rightPanel);
+
+        return pizzaTypePanel;
+    }
+
+    private JPanel initializePizzaSizePanel()
+    {
+        JPanel pizzaSizePanel = new JPanel();
+        pizzaSizePanel.setLayout(new BorderLayout());
+        pizzaSizePanel.setBorder(new EmptyBorder(40, 10, 40, 10));
+
+        String[] pizzaSizes = {"Small ($8)", "Medium ($12)", "Large ($16)", "Super ($20)"};
+        JComboBox pizzaSizeDropdown = new JComboBox(pizzaSizes);
+
+        pizzaSizePanel.add(pizzaSizeDropdown, BorderLayout.CENTER);
+
+        return pizzaSizePanel;
+    }
+
+    private JPanel initializeCrustTypePanel()
+    {
+        JPanel crustTypePanel = new JPanel();
+        crustTypePanel.setLayout(new GridLayout(3, 1));
+
+        ButtonGroup crustTypeButtons = new ButtonGroup();
+
+        JRadioButton thinCrustTypeButton = new JRadioButton("Thin Crust");
+        crustTypeButtons.add(thinCrustTypeButton);
+
+        JRadioButton regularCrustTypeButton = new JRadioButton("Regular Crust");
+        regularCrustTypeButton.setSelected(true);
+        crustTypeButtons.add(regularCrustTypeButton);
+
+        JRadioButton deepDishCrustTypeButton = new JRadioButton("Deep Dish Crust");
+        crustTypeButtons.add(deepDishCrustTypeButton);
+
+        crustTypePanel.add(thinCrustTypeButton);
+        crustTypePanel.add(regularCrustTypeButton);
+        crustTypePanel.add(deepDishCrustTypeButton);
+
+        return crustTypePanel;
+    }
+
+    private JPanel initializePizzaToppingsPanel()
+    {
+        JPanel pizzaToppingsPanel = new JPanel();
+        pizzaToppingsPanel.setLayout(new GridLayout(3, 2));
+        pizzaToppingsPanel.setBorder(BorderFactory.createTitledBorder(
+                BorderFactory.createLoweredBevelBorder(),
+                "Choose Your Pizza Toppings (Each $1):"
+        ));
+
+        JCheckBox baconToppingBox = new JCheckBox("Bacon");
+        pizzaToppingsPanel.add(baconToppingBox);
+
+        JCheckBox sausageToppingBox = new JCheckBox("Sausage");
+        pizzaToppingsPanel.add(sausageToppingBox);
+
+        JCheckBox appleToppingBox = new JCheckBox("Apple Slices");
+        pizzaToppingsPanel.add(appleToppingBox);
+
+        JCheckBox dragonToppingBox = new JCheckBox("Dragon Teeth");
+        pizzaToppingsPanel.add(dragonToppingBox);
+
+        JCheckBox extraCheeseToppingBox = new JCheckBox("Extra Cheese");
+        pizzaToppingsPanel.add(extraCheeseToppingBox);
+
+        JCheckBox alfredoToppingBox = new JCheckBox("Alfredo Sauce");
+        pizzaToppingsPanel.add(alfredoToppingBox);
+
+        return pizzaToppingsPanel;
+    }
+
+    private JPanel initializeResultsPanel()
+    {
+        JPanel resultsPanel = new JPanel();
+        resultsPanel.setLayout(new BorderLayout());
+        resultsPanel.setBorder(BorderFactory.createLoweredBevelBorder());
+
+        this.resultsText = new JTextArea();
+        resultsPanel.add(this.resultsText, BorderLayout.CENTER);
+
+        return resultsPanel;
+    }
+
+    private JPanel initializeButtonsPanel()
+    {
+        JPanel buttonsPanel = new JPanel();
+        buttonsPanel.setLayout(new GridLayout(1 ,3));
+        buttonsPanel.setBorder(BorderFactory.createLoweredBevelBorder());
+
+//        JButton orderButton = initializeOrderButton();
+//        JButton clearButton = initializeClearButton();
+//        JButton quitButton = initializeQuitButton();
+
+        return buttonsPanel;
     }
 }
