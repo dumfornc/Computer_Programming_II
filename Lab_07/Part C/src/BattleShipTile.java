f34c4087628a12572244607857ff33b6bfe7cbed
dumfornc <dumfornc@mail.uc.edu>
2025-11-05 02:39:12 -0500

    [Lab07C] Made BattleShipTile.java enabled status change with current state. Added a couple comments.

--- Lab_07/Part C/src/BattleShipTile.java
+++ Lab_07/Part C/src/BattleShipTile.java
@@ -2,15 +2,17 @@ import javax.swing.*;
 
 public class BattleShipTile extends JButton
 {
+    // Constants representing what image should be displayed and what the state of the button is
     public static final int TILE_IS_EMPTY = 0;
     public static final int TILE_IS_MISS = 1;
     public static final int TILE_IS_HIT = 2;
 
+    // Constants of the images used by BattleShipTiles so that only one image icon is in memory for each image
     private static final ImageIcon emptyIcon = new ImageIcon("imgs\\emptyIcon.jpg");
     private static final ImageIcon missIcon = new ImageIcon("imgs\\missIcon.jpg");
-    private static final ImageIcon hitIcon = new ImageIcon("imgs\\hitTile.jpg");
+    private static final ImageIcon hitIcon = new ImageIcon("imgs\\hitIcon.jpg");
 
-    private int tileState = TILE_IS_EMPTY;
+    private int tileState;
 
     private final int row;
     private final int col;
@@ -42,16 +44,22 @@ public class BattleShipTile extends JButton
         {
             this.tileState = TILE_IS_EMPTY;
             setIcon(emptyIcon);
+
+            setEnabled(true);
         }
         else if(newState == TILE_IS_MISS)
         {
             this.tileState = TILE_IS_MISS;
             setIcon(missIcon);
+
+            setEnabled(false);
         }
         else if(newState == TILE_IS_HIT)
         {
             this.tileState = TILE_IS_HIT;
             setIcon(hitIcon);
+
+            setEnabled(false);
         }
     }
 }
