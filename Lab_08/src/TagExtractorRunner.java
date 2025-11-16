79ca072052978f87e4fddec2c6e8910cc03e3e89
dumfornc <dumfornc@mail.uc.edu>
2025-11-15 20:44:01 -0500

    [Lab08] Created TagExtractorRunner.java

---
+++ Lab_08/src/TagExtractorRunner.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.io.File;
+import java.util.Map;
+
+public class TagExtractorRunner
+{
+    public static void main(String[] args) throws java.io.FileNotFoundException
+    {
+        JFrame mainUI = new TagExtractorFrame();
+        mainUI.setVisible(true);
+    }
+}
